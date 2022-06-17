39960aa5cf173cffff677b8be32adae834a41209
Jake Benilov <benilov@gmail.com>
2022-06-17 15:51:27 +0100

    Add check to ensure node command worked

--- config.rb
+++ config.rb
@@ -2,5 +2,6 @@ require "govuk_tech_docs"
 require_relative "lib/extensions/govuk_tech_docs/api_reference/api_reference_renderer.rb"
 
 system 'npx openapi-merge-cli'
+raise "Cannot run `npx openapi-merge-cli`. Check that Node is installed and matches the version in .nvmrc" if $?.exitstatus != 0
 
 GovukTechDocs.configure(self)
